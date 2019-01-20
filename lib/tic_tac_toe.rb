d38e1b571da64e3d0a2dc030ffa132625ce437f8
bonillamiranda3 <bonillamiranda3@gmail.com>
2019-01-20 03:29:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -130,7 +130,7 @@ def play(board)
   until over?(board) || won?(board)
     turn(board)
   end
-  puts "Cat's Game!" if draw?(board)
+  puts "Cat\'s Game!" if draw?(board)
   puts "Congratulations X!" if winner(board) == "X"
   puts "Congratulations O!" if winner(board) == "O"
 end
