2b9505ef0a924d61424b278417d94bf7f6d7d856
bonillamiranda3 <bonillamiranda3@gmail.com>
2019-01-20 04:31:05 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -95,7 +95,7 @@ def display_board(board)
 end
 
 def valid_move?(board, index)
-  if index.to_i between?(0, 8)
+  if index.to_i.between?(0, 8)
     return true if board[index] == " "
   end
   return false
